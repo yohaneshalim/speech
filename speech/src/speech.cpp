09cc847e4454a1c3d14f551b41f9374422f1daf2
kenzanin <kenzanin@gmail.com>
2021-08-02 18:44:16 +0700

    add try catch

--- speech/src/speech.cpp
+++ speech/src/speech.cpp
@@ -18,6 +18,18 @@
 #include "world/harvest.h"
 #include "world/stonemask.h"
 
+static const std::map<int, std::string> errCode{
+    {0000, "success"},
+    {1000, "Error : file not found"},
+    {1001, "Error : file cannot be read"},
+    {1002, "Error : file is not on correct format"},
+    {2000, "Error : no speech detected"},
+    {2001, "Error : cannot calculate pitch 1. Reason : ..."},
+    {2002, "Error : cannot calculate pitch 2. Reason : ..."},
+    {2003, "Error : cannot calculate pitch 3. Reason : ..."},
+    {2004, "Error : cannot calculate pitch 4. Reason : ..."},
+    {3000, "Error : Memory Allocation Error"}};
+
 struct _wavFile {
  public:
   const char *fileName{};
@@ -27,16 +39,27 @@ struct _wavFile {
   const double *buf{};
   ~_wavFile() { delete[] buf; }
   _wavFile(const char *file = {}) : fileName(file) {
-    length = GetAudioLength(fileName);
-    if (length == 0 || length == -1) {
-      std::cerr << "error mendapatkan wav length atau salah format\n";
-      exit(-2);
+    try {
+      length = GetAudioLength(fileName);
+      if (length == 0 || length == -1) {
+        throw 1002;
+      }
+    } catch (int e) {
+      auto x = jsonResult.at("examples").at(0);
+      x.at("status") = 1002;
+      x.at("comment") = errCode.at(1002);
+      std::cerr << 1002 << " " << errCode.at(1002) << "\n";
+      throw;
     }
+
     try {
       buf = new const double[length]{};
     } catch (std::bad_alloc &e) {
-      std::cerr << "alokasi memory gagal" << e.what() << "\n";
-      exit(-4);
+      std::cerr << 3000 << " " << errCode.at(3000) << " " << e.what() << "\n";
+      auto x = jsonResult.at("examples").at(0);
+      x.at("status") = 3000;
+      x.at("comment") = errCode.at(3000) + e.what();
+      throw 3000;
     }
     wavread(fileName, &fs, &nbit, const_cast<double *>(buf));
   }
@@ -55,8 +78,11 @@ struct _f0 {
       f0 = new double[numOfFrame]{};
       temporalPossition = new double[numOfFrame]{};
     } catch (std::bad_alloc &e) {
-      std::cerr << "alokasi memory gagal" << e.what() << "\n";
-      exit(-4);
+      std::cerr << 3000 << " " << errCode.at(3000) << " " << e.what() << "\n";
+      auto x = jsonResult.at("examples").at(0);
+      x.at("status") = 3000;
+      x.at("comment") = errCode.at(3000) + e.what();
+      throw 3000;
     }
   }
   ~_f0() {
@@ -122,23 +148,47 @@ double getPitch4(const double *dat, int const dat_length) {
 }
 
 int __PitchAnalyzer(const char *fileName) {
-  _wavFile wav(fileName);
+  if (std::FILE *file = std::fopen(fileName, "r")) {
+    std::fclose(file);
+  } else {
+    std::cerr << errCode.at(1000) << "\n";
+    auto &x = jsonResult.at("examples").at(0);
+    x.at("status") = 1000;
+    x.at("comment") = errCode.at(1000);
+    return 1000;
+  }
+  _wavFile *wav{};
+  try {
+    wav = new _wavFile(fileName);
+  } catch (int e) {
+    return e;
+  }
+
   DioOption option{};
   InitializeDioOption(&option);
 
   // F0 analysis
-  _f0 f0(GetSamplesForDIO(wav.fs, wav.length, option.frame_period));
-  Dio(wav.buf, wav.length, wav.fs, &option, f0.temporalPossition, f0.f0);
-  std::future<double> ret1 = std::async(&getPitch1, f0.f0, f0.numOfFrame);
-  std::future<double> ret2 = std::async(&getPitch2, f0.f0, f0.numOfFrame);
-  std::future<double> ret3 = std::async(&getPitch3, f0.f0, f0.numOfFrame);
-  std::future<double> ret4 = std::async(&getPitch4, f0.f0, f0.numOfFrame);
+  _f0 *f0{};
+  try {
+    f0 = new _f0(GetSamplesForDIO(wav->fs, wav->length, option.frame_period));
+  } catch (int e) {
+    delete f0;
+    return e;
+  }
+
+  Dio(wav->buf, wav->length, wav->fs, &option, f0->temporalPossition, f0->f0);
+  std::future<double> ret1 = std::async(&getPitch1, f0->f0, f0->numOfFrame);
+  std::future<double> ret2 = std::async(&getPitch2, f0->f0, f0->numOfFrame);
+  std::future<double> ret3 = std::async(&getPitch3, f0->f0, f0->numOfFrame);
+  std::future<double> ret4 = std::async(&getPitch4, f0->f0, f0->numOfFrame);
 
   jsonResult.at("examples").at(0).at("pitch1") = ret1.get();
   jsonResult.at("examples").at(0).at("pitch2") = ret2.get();
   jsonResult.at("examples").at(0).at("pitch3") = ret3.get();
   jsonResult.at("examples").at(0).at("pitch4") = ret4.get();
 
+  delete f0;
+  delete wav;
   return {};
 }
 
@@ -148,27 +198,21 @@ extern "C" {
 
 DLLEXPORT
 int ADDCALL PitchAnalyzer(char *const fileName, char *const dst) {
-  if (std::FILE *file = std::fopen(fileName, "r")) {
-    std::fclose(file);
-  } else {
-    std::cerr << "error file " << fileName << " tidak ditemukan\n";
-    std::exit(-1);
-  }
-  __PitchAnalyzer(fileName);
+  auto err = __PitchAnalyzer(fileName);
+  if (err != 0) {
+    std::strncpy(dst, jsonResult.dump().c_str(), jsonResult.dump().length());
+    return err;
+  };
+
   std::strncpy(dst, jsonResult.dump().c_str(), jsonResult.dump().length());
   return 0;
 }
 
 DLLEXPORT
 char *ADDCALL PitchAnalyzer2(char const *fileName) {
-  if (std::FILE *file = std::fopen(fileName, "r")) {
-    std::fclose(file);
-  } else {
-    std::cerr << "error file " << fileName << " tidak ditemukan\n";
-    std::exit(-1);
-  }
-  __PitchAnalyzer(fileName);
   char *json_return = new char[jsonResult.dump().length()]{};
+
+  __PitchAnalyzer(fileName);
   std::strncpy(json_return, jsonResult.dump().c_str(),
                jsonResult.dump().length());
   return json_return;
